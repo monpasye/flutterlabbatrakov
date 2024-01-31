d5a87476e174bf8c40556d3a747aa82eeb442402
monpasye <dlafanre@gmail.com>
2024-01-31 19:41:58 +0600

    AppBar, Avatar, Name

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,53 @@
+import 'package:flutterlabbatrakov/icons.dart';
 import 'package:flutter/material.dart';
 
+import 'appbar/avatar.dart';
+import 'appbar/name.dart';
+
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    Color iconColor = const Color.fromRGBO(8, 166, 82, 1.0);
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          leading: Padding(
+          padding: const EdgeInsets.only(left: 14.0),
+          child: Icon(
+            MyFlutterApp.shape,
+            color: iconColor),
+          ),
+          elevation: 0,
+          actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 14.0),
+            child: Icon(
+              MyFlutterApp.combined_shape,
+              color: iconColor),
+          ),
+          ],
         ),
-      ),
+        body: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: MediaQuery.of(context).size.width / 4,),
+              child: Avatar()
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: MediaQuery.of(context).size.width / 4,
+              top: 20),
+              child: Name()
+            )
+          ]
+        )
+      )
     );
   }
 }
